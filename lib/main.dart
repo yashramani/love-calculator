7e5abd475d95a7d87a1e12778a7247bdd7dd4db6
yashramani <ramaniyash2314@gmail.com>
2022-01-28 14:22:06 +0530

    Initial Commit...

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Love Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
